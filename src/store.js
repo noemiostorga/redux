cfa5bd0172c66887ca070bfef1936f8dd5dd1ce0
Noemi Ostorga <noemiostorga@gmail.com>
2016-08-23 16:11:03 -0700

    added store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 
 // import reducers here
-
+import reducers from 'reducers/reducers';
 // combine reducers
 const reducers = combineReducers({
   // put reducers here
+  reducers
 })
 
 // crerate the store
