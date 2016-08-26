9927a450e2de9d97bd7aaf3c80f27201df10378c
Noemi Ostorga <noemiostorga@gmail.com>
2016-08-26 15:53:37 -0700

    implemented store from scratch

--- src/main.js
+++ src/main.js
@@ -219,32 +219,52 @@
 // noprotect
 
 
-const Counter = (state=10 , action) => {
-  switch(action.type){
-    case "ADD":
-      return state + " add ";
-
-    case "DELETE":
-      return (state + 1 ) + console.log("hey");
-
-    default:
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default: 
       return state;
   }
 }
 
-const { createStore } = Redux;
-const store = createStore(Counter);
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+  
+  const getState = () => state;
+  
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+  
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
+  
+  dispatch({});
+  
+  return { getState, dispatch, subscribe };
+};
+
+const store = createStore(counter);
 
 const render = () => {
   document.body.innerText = store.getState();
-}
+};
 
 store.subscribe(render);
 render();
 
-document.addEventListener("click", () => {
-  store.dispatch({type: "DELETE"})
-})
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+});
 
 
 
