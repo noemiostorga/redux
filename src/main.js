1c5484bbf0e8519d3eaa5b9848b6f4d59d4eb015
Noemi Ostorga <noemiostorga@gmail.com>
2016-08-30 19:16:46 -0700

    addded create store in curly braces

--- src/main.js
+++ src/main.js
@@ -26,48 +26,57 @@
 // );
 
 // render(Site, document.getElementById('app'));
-
-const Counter = (state= 0, action) => {
+const counter = (state= 0, action) => {
   switch(action.type){
     case "INCREMENT":
-    return state + 1 ;
+      return state + 1;
 
     case "DECREMENT":
       return state - 1;
 
-    default: 
+    default:
       return state;
+
   }
 }
 
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}> + </button>
+    <button onClick= {onDecrement}> - </button>
+  </div>
+);
 
-const createStore = (reducer) => {
-  let state;
-  let listeners = [];
 
-  const getState = () => state;
+const {createStore} = Redux;
+const store = createStore(counter);
 
-  const dispatch = (action) => {
-    state = reducer(state,action);
-    listeners.forEach(listener => listener());
-  }
-
-  const subscribe = (listener) => {
-    listeners.push(listener);
-    return () => {
-      listeners.filter(l => l !== listener);
-    }
-  }
-
-  dispatch({});
-  return{getState, dispatch, subscribe};
+const render = () => {
+  ReactDOM.render(
+      <Counter 
+        value={store.getState()}
+        onIncrement={() => 
+          store.dispatch({
+            type: "INCREMENT"
+          })
+        }
+          onDecrement={() => 
+          store.dispatch({
+            type: "DECREMENT"
+          })
+        }
+      />,
+      document.getElementById('root')
+    );
 }
 
-
-
-
-
-
+store.subscribe(render);
+render();
 
 
 
